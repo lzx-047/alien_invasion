bed169b7aff7739495e853c5b313557721c4a246
lizixi <lizixi2006@outlook.com>
2020-03-09 16:06:39 +0800

    显示最高分

--- scoreboard.py
+++ scoreboard.py
@@ -29,7 +29,7 @@ class Scoreboard():
 
     def prep_high_score(self):
         high_score=int(round(self.stats.high_score,-1))
-        high_score_str="{;,}".format(high_score)
+        high_score_str="{:,}".format(high_score)
         self.high_score_image=self.font.render(
             high_score_str,True,self.text_color,self.ai_settings.bg_color
         )
