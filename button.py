f528d1cad46354476137ad80323da92d85cd3f5a
lizixi <lizixi2006@outlook.com>
2020-03-22 14:59:00 +0800

    按钮颜色更改

--- button.py
+++ button.py
@@ -8,7 +8,7 @@ class Button():
         self.screen_rect=screen.get_rect()
 
         self.width,self.height=200,50
-        self.button_color=(0,255,0)
+        self.button_color=(0,125,130)
         self.text_color=(255,255,255)
         self.font=pygame.font.SysFont(None,48)
 
